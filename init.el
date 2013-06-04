c3ba03600d859be66ce4c94f9d559f3f4bec5097
mishadoff <mishadoff@gmail.com>
2013-06-04 12:47:51 +0300

    Fixed emacs server and fullscreen under windows

--- init.el
+++ init.el
@@ -68,14 +68,14 @@
 (require 'smex)
 (smex-initialize)
 
-;; Toggle fullscreen mode
-(defun toggle-fullscreen ()
-  (interactive)
-  (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
-                         '(2 "_NET_WM_STATE_MAXIMIZED_VERT" 0))
-  (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
-                         '(2 "_NET_WM_STATE_MAXIMIZED_HORZ" 0)))
-(toggle-fullscreen)
+;; Toggle fullscreen mode (Not working under windows)
+;; (defun toggle-fullscreen ()
+;;   (interactive)
+;;   (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
+;;                          '(2 "_NET_WM_STATE_MAXIMIZED_VERT" 0))
+;;   (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
+;;                          '(2 "_NET_WM_STATE_MAXIMIZED_HORZ" 0)))
+;; (toggle-fullscreen)
 
 
 ;; Key bindings
